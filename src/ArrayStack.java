63dd1fd298f29a03c50be17ac4f9bb78c2e58c2f
diana ramirez <ramid320@gmail.com>
2019-04-10 02:27:50 -0700

    changed stack type to Card card

--- src/ArrayStack.java
+++ src/ArrayStack.java
@@ -1,19 +1,19 @@
-public class ArrayStack <card> {
+public class ArrayStack<Card> {
       
       //storage for the stack
-      private E[] theData;
+      private Card[] theData;
       private int TOS =-1; //indicates nothing is there initially
       private static final int INITIAL_CAPACITY =10;
       
       //default constructor to create stack
       public ArrayStack() {
-            this.theData= (E[]) new Object[INITIAL_CAPACITY]; //cast to E
+            this.theData= (Card[]) new Object[INITIAL_CAPACITY]; //cast to Card
             //this.TOS =1; you can initialize here as well
             
       }
       
-      @Override
-      public E push(E obj) {
+      
+      public Card push(Card card) {
             // adds elements to the stack
             // first check if there is enough space
             if (TOS ==this.theData.length -1) {//this is stack full
@@ -22,18 +22,18 @@ public class ArrayStack <card> {
                   return null; //get out of method
             }
 		/* TOS++; //go to proper index
-		this.theData[TOS] = obj;  // you can have these three or change void to E obj
+		this.theData[TOS] = obj;  // you can have these three or change void to Card obj
 		return;  */
             
-            return (this.theData[++TOS] =obj);
+            return (this.theData[++TOS] =card);
       }
       
-      @Override
-      public E pop() {
+      
+      public Card pop() {
             // this method will delete an element from the stack
             // first check if there is an element to delete
             if(!isEmpty()) {
-                  E temp = this.theData[TOS]; //save the data first
+                  Card temp = this.theData[TOS]; //save the data first
                   TOS--; 						// decrement the TOS
                   return temp;  				//return the element
                   //return this.theData[TOS--]; // alt method
@@ -44,13 +44,13 @@ public class ArrayStack <card> {
             return null;
       }
       
-      @Override
-      public E peek() {
+      
+      public Card peek() {
             // TODO Auto-generated method stub
             return null;
       }
       
-      @Override
+      
       public boolean isEmpty() {
             // check if TOS = -1
             /* if (TOS==-1) {
